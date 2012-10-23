997eef6ecdd7783b9bfeeeeddeb31bfa6deb0e6c
eskerda <eskerda@gmail.com>
2012-10-23 21:38:57 +0200

    Check if app is enabled at start

--- appinfo/app.php
+++ appinfo/app.php
@@ -2,6 +2,7 @@
 
 
 OCP\App::checkAppEnabled('media');
+OCP\App::checkAppEnabled('submedia');
 
 OC::$CLASSPATH['OC_Media_Collection'] = 'apps/media/lib_collection.php';
 OC::$CLASSPATH['OC_Media_Subsonic'] = 'apps/submedia/lib/subsonic.php';
