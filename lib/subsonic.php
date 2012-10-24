980d52d1e7358afa0463eb7fe5e961de56bb9945
eskerda <eskerda@gmail.com>
2012-10-24 02:16:31 +0200

    Fix formatting on subsonic lib docs

--- lib/subsonic.php
+++ lib/subsonic.php
@@ -25,269 +25,251 @@
 
 http://www.subsonic.org/pages/api.jsp
 
-Param 	Required 	Default 	Comment
-u		Yes 					The username.
-p		Yes						The password, either in clear text or 
-								hex-encoded with a "enc:" prefix.
-v		Yes						The protocol version implemented by the 
-								client, i.e., the version of the 
-								subsonic-rest-api.xsd schema used (see below).
-c		Yes						A unique string identifying the client 
-								application.
-f		No			xml			Request data to be returned in this format. 
-								Supported values are "xml", "json" (since 1.4.0) 
-								and "jsonp" (since 1.6.0). If using jsonp,
-								specify name of javascript callback function 
-								using a callback parameter.
-Code	Description
-0	A generic error.
-10	Required parameter is missing.
-20	Incompatible Subsonic REST protocol version. Client must upgrade.
-30	Incompatible Subsonic REST protocol version. Server must upgrade.
-40	Wrong username or password.
-50	User is not authorized for the given operation.
-60	The trial period for the Subsonic server is over. Please donate to get 
-	a license key. Visit subsonic.org for details.
-70	The requested data was not found.
+u -> username
+p -> password: clear | enc:prefix (hexencoded)
+v -> version
+c -> client
++f -> format: xml, json, jsonp
+
+Error codes
+===========
+0   A generic error.
+10  Required parameter is missing.
+20  Incompatible Subsonic REST protocol version. Client must upgrade.
+30  Incompatible Subsonic REST protocol version. Server must upgrade.
+40  Wrong username or password.
+50  User is not authorized for the given operation.
+60  The trial period for the Subsonic server is over. Please donate to get 
+    a license key. Visit subsonic.org for details.
+70  The requested data was not found.
 
 ***/
 
 class OC_MEDIA_SUBSONIC{
 
-	var $user = false;
-	var $version = false;
-	var $client = false;
-	var $format = 'xml';
+    var $user = false;
+    var $version = false;
+    var $client = false;
+    var $format = 'xml';
 
-	public static $formatWhiteList = array(
-		'xml','json','jsonp'
-	);
+    public static $formatWhiteList = array(
+        'xml','json','jsonp'
+    );
 
-	public static function getFormat($params){
-		$format = (isset($params['f']))?$params['f']:'xml';
-		if (!in_array($format, OC_MEDIA_SUBSONIC::$formatWhiteList)){
-			$format = 'xml';
-		}
-		return $format;
-	}
+    public static function getFormat($params){
+        $format = (isset($params['f']))?$params['f']:'xml';
+        if (!in_array($format, OC_MEDIA_SUBSONIC::$formatWhiteList)){
+            $format = 'xml';
+        }
+        return $format;
+    }
 
-	public function __construct($params){
-		$username = (isset($params['u']))?$params['u']:false;
-		$password = (isset($params['p']))?$params['p']:false;
-		$version = (isset($params['v']))?$params['v']:false;
-		$client = (isset($params['c']))?$params['c']:false;
-		$format = (isset($params['f']))?$params['f']:'xml';
-		
-		if (!$username || !$password || !$version || !$client){
-			throw new Exception("Required string parameter not present", 10);
-		}
+    public function __construct($params){
+        $username = (isset($params['u']))?$params['u']:false;
+        $password = (isset($params['p']))?$params['p']:false;
+        $version = (isset($params['v']))?$params['v']:false;
+        $client = (isset($params['c']))?$params['c']:false;
+        $format = (isset($params['f']))?$params['f']:'xml';
+        
+        if (!$username || !$password || !$version || !$client){
+            throw new Exception("Required string parameter not present", 10);
+        }
 
-		if (!in_array($format, OC_MEDIA_SUBSONIC::$formatWhiteList)){
-			throw new Exception("Format not allowed", 10);
-		}
+        if (!in_array($format, OC_MEDIA_SUBSONIC::$formatWhiteList)){
+            throw new Exception("Format not allowed", 10);
+        }
 
-		if($this->checkAuth($username, $password)){
-			$this->user = $username;
-			$this->version = $version;
-			$this->client = $client;
-			$this->format = $format;
-			return $this;	
-		} else {
-			throw new Exception("Wrong username or password", 40);
-		}
-	}
+        if($this->checkAuth($username, $password)){
+            $this->user = $username;
+            $this->version = $version;
+            $this->client = $client;
+            $this->format = $format;
+            return $this;   
+        } else {
+            throw new Exception("Wrong username or password", 40);
+        }
+    }
 
-	private function checkAuth($user, $password){
+    private function checkAuth($user, $password){
 
-		// Password may be clear or hex encoded (with enc: prefix)
-		if (substr($password,0,4)=="enc:"){
-			$password = PREG_REPLACE(
-				"'([\S,\d]{2})'e","chr(hexdec('\\1'))",substr($password,4)
-			);
-		}
-		$password = hash('sha256', $password);
-		$query=OCP\DB::prepare("SELECT user_id, user_password_sha256 from *PREFIX*media_users WHERE user_id=?");
-		$users=$query->execute(array($user))->fetchAll();
-		if (count($users)>0){
-			$auth = $users[0]['user_password_sha256'];
-			if ($auth == $password){
-				OC_Media_Collection::$uid=$users[0]['user_id'];
-				OC_User::setUserId($users[0]['user_id']);
-				return true;
-			}
-			return false;
-		}
-		return false;
-	}
+        // Password may be clear or hex encoded (with enc: prefix)
+        if (substr($password,0,4)=="enc:"){
+            $password = PREG_REPLACE(
+                "'([\S,\d]{2})'e","chr(hexdec('\\1'))",substr($password,4)
+            );
+        }
+        $password = hash('sha256', $password);
+        $query=OCP\DB::prepare("SELECT user_id, user_password_sha256 from *PREFIX*media_users WHERE user_id=?");
+        $users=$query->execute(array($user))->fetchAll();
+        if (count($users)>0){
+            $auth = $users[0]['user_password_sha256'];
+            if ($auth == $password){
+                OC_Media_Collection::$uid=$users[0]['user_id'];
+                OC_User::setUserId($users[0]['user_id']);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
 
-	/**
-	 * @return 	Returns all configured top-level music folders. Takes no 
-	 *			extra parameters.
-	 */
-	public function getMusicFolders(){
-		/*	To day, there's no support for music folders in owncloud,
-			In that sense, we just return a virtual folder with the
-			username.
+    /**
+     * @return  Returns all configured top-level music folders. Takes no 
+     *          extra parameters.
+     */
+    public function getMusicFolders(){
+        /*  To day, there's no support for music folders in owncloud,
+            In that sense, we just return a virtual folder with the
+            username.
 
-			In subsonic, this structures content as:
-			 - Podcasts
-			 - Music
-			 - ...
+            In subsonic, this structures content as:
+             - Podcasts
+             - Music
+             - ...
 
-			In the future we could, for instance, separate shared
-			music into different folders.
-		*/
-		return array(
-			'musicFolders' => array(
-					'musicFolder' => array(
-						'id' => 0,
-						'name' => $this->user,
-					)
-			)
-		);
-	}
+            In the future we could, for instance, separate shared
+            music into different folders.
+        */
+        return array(
+            'musicFolders' => array(
+                    'musicFolder' => array(
+                        'id' => 0,
+                        'name' => $this->user,
+                    )
+            )
+        );
+    }
 
-	/**
-	 * @brief Returns an indexed structure of all artists.
-	 * @param string optional $musicFolderId If specified, only return artists in the music folder with the given ID. 
-	 * @param int optional $ifModifiedSince If specified, only return a result if the artist collection has changed since the given time (in milliseconds since 1 Jan 1970).
-	 * @return associative array
-	 */
-	public function getIndexes(){
-		/** We want something similar to this mess...
-		<indexes lastModified="1347639481261">
-			<shortcut name="Podcast" id="920"/>
-			<index name="A">
-			<artist name="Antigona" id="937"/>
-			<artist name="Aygan" id="926"/>
-			</index>
-			<index name="B">
-			<artist name="bilk" id="939"/>
-			<artist name="Binaerpilot" id="931"/>
-			<artist name="Brad Sucks" id="935"/>
-			</index>
-			...
-		**/
-		$artists = OC_Media_Collection::getArtists();
+    /**
+     * @brief Returns an indexed structure of all artists.
+     * @param string optional $musicFolderId If specified, only return artists in the music folder with the given ID. 
+     * @param int optional $ifModifiedSince If specified, only return a result if the artist collection has changed since the given time (in milliseconds since 1 Jan 1970).
+     * @return associative array
+     */
+    public function getIndexes(){
+        /** We want something similar to this mess...
+        <indexes lastModified="1347639481261">
+            <shortcut name="Podcast" id="920"/>
+            <index name="A">
+            <artist name="Antigona" id="937"/>
+            <artist name="Aygan" id="926"/>
+            </index>
+            <index name="B">
+            <artist name="bilk" id="939"/>
+            <artist name="Binaerpilot" id="931"/>
+            <artist name="Brad Sucks" id="935"/>
+            </index>
+            ...
+        **/
+        $artists = OC_Media_Collection::getArtists();
 
-		// TODO: Find a way to get the last modified time
-		$response = array(
-			'indexes' => array(
-				'lastModified' => strval(round(microtime(true)*100)),
-				'index' => array()
-			)
-		);
+        // TODO: Find a way to get the last modified time
+        $response = array(
+            'indexes' => array(
+                'lastModified' => strval(round(microtime(true)*100)),
+                'index' => array()
+            )
+        );
 
-		foreach ($artists as $artist){
-			$name = $artist['artist_name'];
-			if (preg_match('/^[a-zA-Z]*$/',$name[0]) > 0){
-				// starts with allowed char
-				$letter = strtoupper($name[0]);
-			} else {
-				// should be grouped under #
-				$letter = '#';
-			}
-			if (!isset($response['indexes']['index'][$letter]))
-				$response['indexes']['index'][$letter] = array();
+        foreach ($artists as $artist){
+            $name = $artist['artist_name'];
+            if (preg_match('/^[a-zA-Z]*$/',$name[0]) > 0){
+                // starts with allowed char
+                $letter = strtoupper($name[0]);
+            } else {
+                // should be grouped under #
+                $letter = '#';
+            }
+            if (!isset($response['indexes']['index'][$letter]))
+                $response['indexes']['index'][$letter] = array();
 
-			$response['indexes']['index'][$letter][] = array(
-				'id' => 'artist_'.$artist['artist_id'],
-				'name' => $artist['artist_name']);
-		}
-		return $response;
-	}
+            $response['indexes']['index'][$letter][] = array(
+                'id' => 'artist_'.$artist['artist_id'],
+                'name' => $artist['artist_name']);
+        }
+        return $response;
+    }
 
-	/**
-	 * @brief 	Returns a listing of all files in a music directory. Typically 
-	 * 			used to get list of albums for an artist, or list of songs for 
-	 * 			an album.
-	 *
-	 * @param int $id required 	A string which uniquely identifies the music 
-	 *							folder. Obtained by calls to getIndexes or 
-	 *							getMusicDirectory.
-	 * @return 	Returns an associative array with a nested 
-	 *			<directory> element on success.
-	 */
-	function getMusicDirectory($params){
-			$id = (isset($params['id']))?$params['id']:false;
-			if (!$id){
-				throw new Exception('Required string parameter \'id\' not present', 10);
-			}
-			$sid = split('_', $id);
-			switch($sid[0]){
-				case 'album':
-					$album_id = $sid[1];
+    function getMusicDirectory($params){
+            $id = (isset($params['id']))?$params['id']:false;
+            if (!$id){
+                throw new Exception('Required string parameter \'id\' not present', 10);
+            }
+            $sid = split('_', $id);
+            switch($sid[0]){
+                case 'album':
+                    $album_id = $sid[1];
 
-					$album = OC_Media_Collection::getAlbumName($album_id);
-					$songs = OC_Media_Collection::getSongs(0, $album_id);
+                    $album = OC_Media_Collection::getAlbumName($album_id);
+                    $songs = OC_Media_Collection::getSongs(0, $album_id);
 
-					$response = array(
-						'directory' => array(
-							'id' => $id,
-							'name' => $album,
-							'child' => array()
-						)
-					);
-					foreach ($songs as $song){
-						if (!isset($artist)){
-							$artist = OC_Media_Collection::getArtistName($song['song_artist']);
-						}
-						$response['directory']['child'][] = array(
-							'id' => $song['song_id'],
-							'parent' => $id,
-							'title' => $song['song_name'],
-							'album' => $album,
-							'artist' => $artist,
-							'isDir' => false,
-							'coverArt' => 'album_'.$album_id,
-							//'created' =>
-							'duration' => $song['song_length'],
-							'bitRate' => round($song['song_size'] / $song['song_length'] * 0.008),
-							'track' => $song['song_track'],
-							//'year' =>
-							//'genre' => 
-							'size' => $song['song_size'],
-							'suffix' => 'mp3',
-							'contentType' => 'audio/mpeg',
-							'isVideo' => false,
-							'path' => sprintf('%s/%s/%d - %s.mp3',$artist,$album,$song['song_track'],$song['song_name']),
-							'albumId' => $album_id,
-							'artistId' => $song['song_artist'],
-							'type' => 'music'
-						);
-					}
-					break;
-				case 'artist':
-					$artist_id = $sid[1];
+                    $response = array(
+                        'directory' => array(
+                            'id' => $id,
+                            'name' => $album,
+                            'child' => array()
+                        )
+                    );
+                    foreach ($songs as $song){
+                        if (!isset($artist)){
+                            $artist = OC_Media_Collection::getArtistName($song['song_artist']);
+                        }
+                        $response['directory']['child'][] = array(
+                            'id' => $song['song_id'],
+                            'parent' => $id,
+                            'title' => $song['song_name'],
+                            'album' => $album,
+                            'artist' => $artist,
+                            'isDir' => false,
+                            'coverArt' => 'album_'.$album_id,
+                            //'created' =>
+                            'duration' => $song['song_length'],
+                            'bitRate' => round($song['song_size'] / $song['song_length'] * 0.008),
+                            'track' => $song['song_track'],
+                            //'year' =>
+                            //'genre' => 
+                            'size' => $song['song_size'],
+                            'suffix' => 'mp3',
+                            'contentType' => 'audio/mpeg',
+                            'isVideo' => false,
+                            'path' => sprintf('%s/%s/%d - %s.mp3',$artist,$album,$song['song_track'],$song['song_name']),
+                            'albumId' => $album_id,
+                            'artistId' => $song['song_artist'],
+                            'type' => 'music'
+                        );
+                    }
+                    break;
+                case 'artist':
+                    $artist_id = $sid[1];
 
-					$artist = OC_Media_Collection::getArtistName($artist_id);
-					$albums = OC_Media_Collection::getAlbums($artist_id);
+                    $artist = OC_Media_Collection::getArtistName($artist_id);
+                    $albums = OC_Media_Collection::getAlbums($artist_id);
 
-					$response = array(
-						'directory' => array(
-							'id'=>$id,
-							'name'=>$artist,
-							'child'=>array()
-						)
-					);
-					foreach ($albums as $album){
-						$response['directory']['child'][] = array(
-							'id' => 'album_'.$album['album_id'],
-							'parent' => $id,
-							'title' => $album['album_name'],
-							'artist' => $artist,
-							'isDir' => true,
-							'coverArt' => 'album_'.$album['album_id'],
-							//'created' => 
-							//'userRating' =>
-							//'averageRating' =>
-						);
-					}
-					break;
-				default:
-					throw new Exception('I don\'t know what am I doing...',0);
-			}
+                    $response = array(
+                        'directory' => array(
+                            'id'=>$id,
+                            'name'=>$artist,
+                            'child'=>array()
+                        )
+                    );
+                    foreach ($albums as $album){
+                        $response['directory']['child'][] = array(
+                            'id' => 'album_'.$album['album_id'],
+                            'parent' => $id,
+                            'title' => $album['album_name'],
+                            'artist' => $artist,
+                            'isDir' => true,
+                            'coverArt' => 'album_'.$album['album_id'],
+                            //'created' => 
+                            //'userRating' =>
+                            //'averageRating' =>
+                        );
+                    }
+                    break;
+                default:
+                    throw new Exception('I don\'t know what am I doing...',0);
+            }
 
-			return $response;
-	}
+            return $response;
+    }
 }
