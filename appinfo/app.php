d6a3476124a5085463f5041079cd4f9284f2abb3
eskerda <eskerda@gmail.com>
2012-11-07 11:12:57 +0100

    Remove navigation entry of the app
    There's no need for a link on the main sidebar

--- appinfo/app.php
+++ appinfo/app.php
@@ -7,11 +7,3 @@ OCP\App::checkAppEnabled('submedia');
 OC::$CLASSPATH['OC_Media_Collection'] = 'apps/media/lib_collection.php';
 OC::$CLASSPATH['OC_Media_Subsonic'] = 'apps/submedia/lib/subsonic.php';
 OC::$CLASSPATH['OC_Media_Playlist'] = 'apps/submedia/lib/playlist.php';
-
-OCP\App::addNavigationEntry( array(
-    'id' => 'submedia',
-    'order' => 74,
-    'href' => OCP\Util::linkTo( 'submedia', 'index.php' ),
-    'name' => 'SubMedia',
-    'icon' => OCP\Util::imagePath('core', 'places/music.svg')
-));
