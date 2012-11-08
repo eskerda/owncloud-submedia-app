d677cbc9eef4bcb81f82d0eb90dcae1e52048c5c
eskerda <eskerda@gmail.com>
2012-11-08 08:19:15 +0100

    Change APPROOT to SERVERROOT 5.0.8 - 5.4.1

--- remote.php
+++ remote.php
@@ -7,4 +7,4 @@ OCP\App::checkAppEnabled('submedia');
 $RUNTIME_APPTYPES=array('filesystem','authentication');
 OC_App::loadApps($RUNTIME_APPTYPES);
 
-require_once(OC::$APPSROOT . '/apps/submedia/server/submedia.php');
+require_once(OC::$SERVERROOT . '/apps/submedia/server/submedia.php');
