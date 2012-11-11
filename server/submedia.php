ec9abe1c6ce1121443bf0f77010a73aa9af116b9
eskerda <eskerda@gmail.com>
2012-11-11 16:39:17 +0100

    Always fill at least an empty response

--- server/submedia.php
+++ server/submedia.php
@@ -2,9 +2,9 @@
 
 $req_info = pathinfo($_SERVER['PATH_INFO']);
 $action = $req_info['filename'];
+$response = array();
 try {
 	$subsonic = new OC_Media_Subsonic($_REQUEST);
-	$response = array();
 	$userid = OC_User::getUser();
 	switch($action){
 		case 'ping':
@@ -55,7 +55,6 @@ try {
 $tmpl = new OCP\Template("submedia", OC_Media_Subsonic::getFormat($_REQUEST));
 $tmpl->assign('response', $response);
 $tmpl->assign('action', $action);
-$tmpl->assign('response', $response);
 if (isset($_REQUEST['callback'])){
 	$tmpl->assign('callback', $_REQUEST['callback']);
 }
