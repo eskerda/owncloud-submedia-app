459079dad5c84d969a087123606e3aed4f1709c4
eskerda <eskerda@gmail.com>
2012-11-29 08:41:50 +0100

    Make xml start at first line of output

--- templates/xml.php
+++ templates/xml.php
@@ -1,3 +1,5 @@
+<?php header ("Content-Type: text/xml"); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <?php
     $errorReporting = false;
 
@@ -16,9 +18,6 @@
                             );
                         });
 ?>
-
-<?php header ("Content-Type: text/xml"); ?>
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <subsonic-response xmlns="http://subsonic.org/restapi" version="1.7.0" status="<?php echo $_['status']; ?>">
 <?php if (isset($_['error'])): ?>
     <error code="<?php echo $_['error']['code']; ?>" message="<?php echo $_['error']['message']; ?>"/>
